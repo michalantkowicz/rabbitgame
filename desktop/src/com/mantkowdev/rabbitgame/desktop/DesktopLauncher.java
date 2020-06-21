bb6772730ddd6a538aad8195844071518aa28c5b
michalantowicz <michal@antkowicz.pl>
2020-06-21 12:45:46 +0200

    initial commit

---
+++ desktop/src/com/mantkowdev/rabbitgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mantkowdev.rabbitgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mantkowdev.rabbitgame.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
